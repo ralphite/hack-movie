125d06ef67d5387fd5d41dbea3b14881b164c795
Wen, Yadong (Ralph) <yawen@microstrategy.com>
2017-01-05 11:34:19 -0500

    add api to post a rate

--- server/app/blueprints/api/ratings.py
+++ server/app/blueprints/api/ratings.py
@@ -9,6 +9,8 @@ from flask import jsonify
 
 from . import api
 
+from time import time
+
 from ... import db
 
 from app.models import Movie, Ratings
@@ -28,3 +30,24 @@ def get_all_ratings():
     return jsonify({
         'ratings': [r.get_rating() for r in ratings]
     })
+
+
+@api.route('/rating/<user_id>/<movie_id>/<rate>', methods=['POST'])
+def rate(user_id, movie_id, rate):
+    ratings = Ratings()
+    ratings.user_id = user_id
+    ratings.movie_id = movie_id
+    ratings.rating = rate
+    ratings.timestamp = str(int(time()))
+
+    try:
+        db.session.add(ratings)
+        db.session.commit()
+        return jsonify({
+            'message': 'successful'
+        }), 200
+    except:
+        db.session.rollback()
+        return jsonify({
+            'message': 'failed'
+        }), 500
